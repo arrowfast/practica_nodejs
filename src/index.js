badf30d2b5e092339a88ccfd7a9481f5477ebf3a
Georyitt <georyitt.gorayeb@inacapmail.cl>
2018-12-24 19:31:46 -0300

    inicializando servidor en src/index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+// Initializations (inicialización) 
+const app = express();
+
+//Settings (Configuraciones)
+app.set('port', process.env.PORT || 4000);
+
+// Middlewares
+app.use(morgan('dev'));
+
+// Global Variables (Variables Globales)
+
+
+//Routes (rutas)
+
+// Public (Publica)
+
+// Starting the server (Inicializacion del servidor)
+app.listen(app.get('port'),() => {
+    console.log('Server on port', app.get('port'));
+});
